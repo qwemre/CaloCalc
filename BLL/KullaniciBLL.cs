c2cd8bca2b1305af9318c4c062973fb6bad63088
emrekaraomeroglu@gmail.com <emrekaraomeroglu@gmail.com>
2023-05-08 01:59:44 +0300

    BLL crud

--- BLL/KullaniciBLL.cs
+++ BLL/KullaniciBLL.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    internal class KullaniciBLL
+    public class KullaniciBLL
     {
     }
 }
--- BLL/YiyecekBLL.cs
+++ BLL/YiyecekBLL.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    internal class YiyecekBLL
+    public class YiyecekBLL
     {
     }
 }

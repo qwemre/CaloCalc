a3e0eea4db0dc3b854e69b0e70925fcd035053c2
Büşra Önder <124676709+bsrndr@users.noreply.github.com>
2023-05-08 23:05:46 +0300

    giriş ekranı son hali

--- CaloCalc/Giris2.Designer.cs
+++ CaloCalc/Giris2.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Giris2";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Giris2));
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(423, 415);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Consolas", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(139, 90);
+            label1.Name = "label1";
+            label1.Size = new Size(179, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Giriş Yap";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Consolas", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(54, 162);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 18);
+            label2.TabIndex = 1;
+            label2.Text = "E-Mail:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Consolas", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(62, 194);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 18);
+            label3.TabIndex = 1;
+            label3.Text = "Şifre:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(124, 194);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Şifreniz";
+            textBox1.Size = new Size(203, 26);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(124, 161);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "E-Mail Adresiniz";
+            textBox2.Size = new Size(203, 26);
+            textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.ForestGreen;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(140, 236);
+            button1.Name = "button1";
+            button1.Size = new Size(160, 42);
+            button1.TabIndex = 3;
+            button1.Text = "Giriş";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Giris2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(423, 415);
+            Controls.Add(panel1);
+            Name = "Giris2";
+            Text = "Giris2";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
     }
 }
